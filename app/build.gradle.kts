c9ee404c92a3de65af104e7df28ea89b06109280
aravindhan182 <aravindhvjjay007@gmail.com>
2025-05-12 14:52:50 +0530

    Added required libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.runtime.livedata.compose)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
 }
